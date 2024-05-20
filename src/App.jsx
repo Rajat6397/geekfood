a99b7e97912448c94e60319e83c0444a3f3df43e
Rajat <rajatsaxena6397@gmail.com>
2024-05-20 23:30:13 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <div className="navbar">
+        <div className="logo">
+        <img src="https://flowbite.com/docs/images/logo.svg"/>
+        <h2>GeekFoods</h2>
+        </div>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Quote</a></li>
+          <li><a href="#">Resturants</a></li>
+          <li><a href="#">Food</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <button>Get started</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
